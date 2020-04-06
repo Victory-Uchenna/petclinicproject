913485d9d22e1b3677503d442c8f6bc4839a7ac2
vuchenna <victory.uchenna@qa.com>
2020-04-06 21:18:18 +0000

    terraform for kubernetes

---
+++ project/README.md
@@ -0,0 +1,1 @@
+# Configuring kubernetes cluster with Terraform
---
+++ project/main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+  project = "pure-fold-266009"
+  region = "europe-west2"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "petclinic"
+  location = "europe-west2"
+
+  remove_default_node_pool = true
+  initial_node_count       = 3
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
